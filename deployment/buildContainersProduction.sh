a737eae022ab12372d88ffc7923765fd5d036bc0
Bruno Pereira <bdpereira@uporto.pt>
2023-02-14 17:26:48 +0000

    improve installation scripts and details

---
+++ deployment/buildContainersProduction.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CURRENT=$(git rev-parse --abbrev-ref HEAD)
+SUPPOSED="production"
+if [ "$CURRENT" = "$SUPPOSED" ]; then
+    export $(grep -v '^#' ../.env | xargs -d '\n') && sed -e "s,\$ESVA_BACKEND,${ESVA_BACKEND},g" ./validador/ui/configs/Configs.json.tmpl > ./validador/ui/configs/Configs.json
+    git pull
+    docker rm -f esvavalidator validador-core validador-ui validatornginx
+    docker-compose up -d --build
+else
+    echo "You are not in the correct branch. Please change to $SUPPOSED branch."
+fi
