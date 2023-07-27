ec95dd7d0ed883a9a2e9caa03d9f582e294268c2
Bruno Pereira <bdpereira@uporto.pt>
2023-07-27 16:34:06 +0100

    test

--- src/controller/statsController.ts
+++ src/controller/statsController.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from 'express'
+// import { Request, Response } from 'express'
 
 // import logs from '../services/logs'
 
 // Use Case to Validate OLAs
-const stats = async (req: Request, res: Response) => {
-  console.log(req.path)
-  res.send('hello')
+const stats = async () => {
+  console.log('hello')
 }
 
 export default { stats }
