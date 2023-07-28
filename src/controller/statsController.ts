f2e3f27aef991d035b145749b483e58f9417ba09
Bruno Pereira <bdpereira@uporto.pt>
2023-07-28 19:33:19 +0100

    add logs

--- src/controller/statsController.ts
+++ src/controller/statsController.ts
@@ -5,6 +5,7 @@ import logs from '../services/logs'
 // Use Case to Validate OLAs
 const stats = async (req: Request, res: Response) => {
   console.log(req.path)
+  console.log(await logs.getLogs())
   res.send(await logs.getLogs())
 }
 
