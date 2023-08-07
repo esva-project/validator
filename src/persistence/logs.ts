cc40705a521b6c151d741400d7b4b696e1cc160e
Bruno Pereira <bdpereira@uporto.pt>
2023-08-07 13:01:04 +0100

    add logs

--- src/persistence/logs.ts
+++ src/persistence/logs.ts
@@ -6,6 +6,8 @@ const pg = pgPromise({})
 
 const db = pg(`postgres://postgres:${process.env.DB_PASS}@db:5432/esva_db`)
 
+console.log(`postgres://postgres:${process.env.DB_PASS}@db:5432/esva_db`);
+
 const checkTableExists = async () => {
   console.log(Number.parseInt(process.env.TIME_STORAGE_LOGS as string))
 
