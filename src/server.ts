2531dd16773128cbe961deca82768e9a52772532
Bruno Pereira <bdpereira@uporto.pt>
2023-08-09 11:08:00 +0100

    add exposed port for logs

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@
 import express from 'express'
 import morgan from 'morgan'
 
+import statsController from './controller/statsController' // Import the statsController module
 import routes from './routes/validationRoutes'
 
 const app = express()
+const logsApp = express() // New Express app for /logs endpoint
 
 /** Logging */
 app.use(morgan('dev'))
@@ -17,11 +19,22 @@ app.use(express.json())
 /** Routes */
 app.use('/', routes.router)
 
+// Define the /logs endpoint on the new app and start on port 4000
+logsApp.get('/logs', statsController.logs)
+
+// Set the port for the /logs app
+const LOGS_PORT = 4000
+logsApp.listen(LOGS_PORT, () => {
+  console.log(`Logs app is running on port ${LOGS_PORT}`)
+})
+
 /** Error handling */
 app.use((_, res) => {
   const { message } = new Error('not found')
   return res.status(404).json({ message })
 })
 
-const PORT = 6060
-app.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
+const MAIN_PORT = 6060
+app.listen(MAIN_PORT, () => {
+  console.log(`Main app is running on port ${MAIN_PORT}`)
+})
