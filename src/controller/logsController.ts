64a6b0903c22b21c212c7a0a0d34421d40c70043
Bruno Pereira <bdpereira@uporto.pt>
2023-08-09 15:08:07 +0100

    logs

--- src/controller/logsController.ts
+++ src/controller/logsController.ts
@@ -5,6 +5,10 @@ import logsService from '../services/logs'
 // Use Case to Validate OLAs
 const logs = async (req: Request, res: Response) => {
   console.log(req.path)
+
+  console.log(req.fields)
+  console.log(req.query)
+
   res.send(await logsService.getLogs())
 }
 
