91848a1aa88e0a548ffde67ad6ea1a85dab24ab4
Bruno Pereira <bdpereira@uporto.pt>
2023-09-29 16:54:51 +0100

    fix instruction copy privacy policy

--- deployment/buildContainersDevelopment.sh
+++ deployment/buildContainersDevelopment.sh
@@ -3,7 +3,7 @@ CURRENT=$(git rev-parse --abbrev-ref HEAD)
 SUPPOSED="development"
 if [ "$CURRENT" = "$SUPPOSED" ]; then
     export $(grep -v '^#' ../.env | xargs -d '\n') && sed -e "s,\$ESVA_BACKEND,${ESVA_BACKEND},g" -e "s,\$IS_EWP_ACTIVE,${IS_EWP_ACTIVE},g" ../validador/ui/configs/Configs.json.tmpl > ../validador/ui/configs/Configs.json
-    cp ../privacy-policy.pdf ../../assets/privacy-policy.pdf
+    cp ../privacy-policy.pdf ../validador/ui/repo/code/src/assets/privacy-policy.pdf
     git pull
     export $(grep -v '^#' ../.env | xargs -d '\n') && sed -e "s,\${HOST},${HOST},g" -e "s,\${DB_PASS},${DB_PASS},g" ../docker-compose.yml.tmpl > ../docker-compose.yml
     docker rm -f esvavalidator validador-core validador-ui validatornginx
