92b8cc3b600fa7eb9b53a30af83d750df9178960
Bruno Pereira <bdpereira@uporto.pt>
2023-11-13 17:11:57 +0000

    logs

--- src/services/ewpValidations/validateInstitutionDataService.ts
+++ src/services/ewpValidations/validateInstitutionDataService.ts
@@ -45,17 +45,15 @@ const validateEWPInstitutionsResponse = async (
 
   // Compare Institution and Mobility Informations
   for (const contact of institutions_response.getContacts()) {
-    if (flow == 2) {
-      console.log('2')
-      console.log(contact.getContactPersonName())
-      console.log(hei_information.getMobilitySignature()?.getName())
+    console.log(contact.getContactPersonName())
+    console.log(hei_information.getMobilitySignature()?.getName())
 
-      console.log(contact.getContactPersonEmail())
-      console.log(hei_information.getMobilitySignature()?.getEmail())
+    console.log(contact.getContactPersonEmail())
+    console.log(hei_information.getMobilitySignature()?.getEmail())
 
-      console.log(contact.getContactPersonRoleDescription())
-      console.log(hei_information.getMobilitySignature()?.getRole())
-    }
+    console.log(contact.getContactPersonRoleDescription())
+    console.log(hei_information.getMobilitySignature()?.getRole())
+
     if (contact.getContactPersonName() === hei_information.getMobilitySignature()?.getName()) {
       mobilityValidation.foundSendingHEIValdiation('LA Signer Name', locationBothAPIs)
     }
