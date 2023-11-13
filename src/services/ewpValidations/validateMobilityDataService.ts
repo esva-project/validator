c86708678b18101c6df8fc017a99cde9cda0eeb1
Bruno Pereira <bdpereira@uporto.pt>
2023-11-13 18:04:25 +0000

    fix

--- src/services/ewpValidations/validateMobilityDataService.ts
+++ src/services/ewpValidations/validateMobilityDataService.ts
@@ -44,13 +44,13 @@ const validateEWPMobilityResponse = async (
     )
 
     for (const signature of response.getSignatures()) {
-      if (signature.getCommonName().toLowerCase() === sending_signature.getName().toLowerCase()) {
+      if (signature.getCommonName().toLowerCase() === sending_signature.getName()) {
         response.foundSendingHEIValdiation('LA Signer Name', location)
       }
       if (signature.getEmail() === sending_signature.getEmail()) {
         response.foundSendingHEIValdiation('LA Signer Email', location)
       }
-      if (signature.getCommonName().toLowerCase() === receiving_signature.getName().toLowerCase()) {
+      if (signature.getCommonName().toLowerCase() === receiving_signature.getName()) {
         response.foundReceivingHEIValdiation('LA Signer Name', location)
       }
       if (signature.getEmail() === receiving_signature.getEmail()) {
