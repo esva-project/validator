7c2f4ec1e1b88df2a0674c0419f93849a955544b
Bruno Pereira <bdpereira@uporto.pt>
2025-09-09 15:44:19 +0100

    log

--- src/model/iiaResponse.ts
+++ src/model/iiaResponse.ts
@@ -96,6 +96,7 @@ class HEIIIA {
   public getContactPersonEmail = () => this.hei['signing-contact']?.['c:email']
   public getContactPersonRole = () => this.hei['signing-contact']?.['c:role-description']._text
   public getOtherContactPersonName() {
+    console.log(this.hei.contact)
     if (this.hei.contact != undefined) {
       console.log(this.hei.contact[0]['contact-name'])
     }
