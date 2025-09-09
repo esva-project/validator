27a38af36783432317effe65418b44bc8fbc06c9
Bruno Pereira <bdpereira@uporto.pt>
2025-09-09 17:46:15 +0100

    add condition

--- src/model/ounitResponse.ts
+++ src/model/ounitResponse.ts
@@ -6,7 +6,7 @@ interface OUnitsInterface {
 }
 
 interface OUnitInterface {
-  ounit: OUnitDetailsInterface
+  ounit?: OUnitDetailsInterface
 }
 
 interface OUnitDetailsInterface {
@@ -25,38 +25,56 @@ class OUnits {
     console.log(JSON.stringify(this.ounit['ounits-response'].ounit))
     console.log(JSON.stringify(ounit))
 
-    if (this.ounit['ounits-response'].ounit.contact == undefined) {
-      this.ounit['ounits-response'].ounit.contact = []
-    } else {
-      if (!Array.isArray(this.ounit['ounits-response'].ounit.contact)) {
-        this.ounit['ounits-response'].ounit.contact = [this.ounit['ounits-response'].ounit.contact]
+    if (this.ounit['ounits-response'].ounit != undefined) {
+      if (this.ounit['ounits-response'].ounit.contact == undefined) {
+        this.ounit['ounits-response'].ounit.contact = []
+      } else {
+        if (!Array.isArray(this.ounit['ounits-response'].ounit.contact)) {
+          this.ounit['ounits-response'].ounit.contact = [
+            this.ounit['ounits-response'].ounit.contact
+          ]
+        }
       }
-    }
 
-    if (!Array.isArray(this.ounit['ounits-response'].ounit.name)) {
-      this.ounit['ounits-response'].ounit.name = [this.ounit['ounits-response'].ounit.name]
+      if (!Array.isArray(this.ounit['ounits-response'].ounit.name)) {
+        this.ounit['ounits-response'].ounit.name = [this.ounit['ounits-response'].ounit.name]
+      }
+      for (const c of this.ounit['ounits-response'].ounit.contact) {
+        c['role-description'] = new StringLang(c['role-description'])
+      }
+      const nameList = []
+      for (const n of this.ounit['ounits-response'].ounit.name) {
+        nameList.push(new StringLang(n))
+      }
+      this.ounit['ounits-response'].ounit.name = nameList
     }
-    for (const c of this.ounit['ounits-response'].ounit.contact) {
-      c['role-description'] = new StringLang(c['role-description'])
-    }
-    const nameList = []
-    for (const n of this.ounit['ounits-response'].ounit.name) {
-      nameList.push(new StringLang(n))
-    }
-    this.ounit['ounits-response'].ounit.name = nameList
   }
 
-  public getOUnitID = () => this.ounit['ounits-response'].ounit['ounit-id']
+  public getOUnitID = () => {
+    if (this.ounit['ounits-response'].ounit != undefined) {
+      return this.ounit['ounits-response'].ounit['ounit-id']
+    }
+    return ''
+  }
   public getOUnitName = () => {
-    const nameArray = this.ounit['ounits-response'].ounit.name
-    return nameArray.some((e) => e.getLang() == 'en')
-      ? (nameArray.find((e) => e.getLang() == 'en') as StringLang)
-      : nameArray[0]
+    if (this.ounit['ounits-response'].ounit != undefined) {
+      const nameArray = this.ounit['ounits-response'].ounit.name
+      return nameArray.some((e) => e.getLang() == 'en')
+        ? (nameArray.find((e) => e.getLang() == 'en') as StringLang)
+        : nameArray[0]
+    }
+    return ''
   }
-  public getOUnitNames = () => this.ounit['ounits-response'].ounit.name
+  public getOUnitNames = () => {
+    if (this.ounit['ounits-response'].ounit != undefined) {
+      return this.ounit['ounits-response'].ounit.name
+    }
+    return ''
+  }
   public getContacts = () => {
     const list: Contact[] = []
     if (
+      this.ounit['ounits-response'].ounit != undefined &&
       this.ounit['ounits-response'].ounit.contact != undefined &&
       this.ounit['ounits-response'].ounit.contact.length > 0
     ) {
